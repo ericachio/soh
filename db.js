468a570ae540274a54de9758360776fe86ba2c3e
Erica Chio <ericanewyork@gmail.com>
2018-11-29 15:35:43 -0500

    please please

--- db.js
+++ db.js
@@ -74,26 +74,26 @@ mongoose.model('Progress', Progress);
 // ---------------------------------------------------
 
 // is the environment variable, NODE_ENV, set to PRODUCTION? 
-// let dbconf;
-// if (process.env.NODE_ENV === 'PRODUCTION') {
-//  // if we're in PRODUCTION mode, then read the configration from a file
-//  // use blocking file io to do this...
-//  const fs = require('fs');
-//  const path = require('path');
-//  const fn = path.join(__dirname, 'config.json');
-//  const data = fs.readFileSync(fn);
+let dbconf;
+if (process.env.NODE_ENV === 'PRODUCTION') {
+ // if we're in PRODUCTION mode, then read the configration from a file
+ // use blocking file io to do this...
+ const fs = require('fs');
+ const path = require('path');
+ const fn = path.join(__dirname, 'config.json');
+ const data = fs.readFileSync(fn);
 
-//  // our configuration file will be in json, so parse it and set the
-//  // conenction string appropriately!
-//  const conf = JSON.parse(data);
-//  dbconf = conf.dbconf;
-// } else {
-//  // if we're not in PRODUCTION mode, then use
-//  //dbconf = 'mongodb://localhost/sohfinalprojconfig';
-//  dbconf = 'mongodb://ebc308:721Babychio!@ds121624.mlab.com:21624/soh';
-// }
+ // our configuration file will be in json, so parse it and set the
+ // conenction string appropriately!
+ const conf = JSON.parse(data);
+ dbconf = conf.dbconf;
+} else {
+ // if we're not in PRODUCTION mode, then use
+ //dbconf = 'mongodb://localhost/sohfinalprojconfig';
+ dbconf = 'mongodb://erica:721ericachio@ds044577.mlab.com:44577/soh';
+}
 
-// mongoose.connect(dbconf, { useNewUrlParser: true });
+mongoose.connect(dbconf, { useNewUrlParser: true });
 
 //____________________________________________________
 // if (process.env.NODE_ENV === 'PRODUCTION') {
@@ -111,28 +111,29 @@ mongoose.model('Progress', Progress);
 //  mongoose.connect(dbconf, { useNewUrlParser: true });
 // } else {
     //lets require/import the mongodb native drivers.
-    var mongodb = require('mongodb');
+    // var mongodb = require('mongodb');
 
-    //We need to work with "MongoClient" interface in order to connect to a mongodb server.
-    var MongoClient = mongodb.MongoClient;
+    // //We need to work with "MongoClient" interface in order to connect to a mongodb server.
+    // var MongoClient = mongodb.MongoClient;
 
-    // Connection URL. This is where your mongodb server is running.
+    // // Connection URL. This is where your mongodb server is running.
 
-    //(Focus on This Variable)
-    var url = 'mongodb://erica:721ericachio@ds044577.mlab.com:44577/soh';      
-    //(Focus on This Variable)
-    // Use connect method to connect to the Server
-      MongoClient.connect(url, function (err, db) {
-      if (err) {
-        console.log('Unable to connect to the mongoDB server. Error:', err);
-      } else {
-        console.log('Connection established to', url);
+    // //(Focus on This Variable)
+    // var url = 'mongodb://erica:721ericachio@ds044577.mlab.com:44577/soh';      
+    // //(Focus on This Variable)
+    // // Use connect method to connect to the Server
+    //   MongoClient.connect(url, function (err, db) {
+    //   if (err) {
+    //     console.log('Unable to connect to the mongoDB server. Error:', err);
+    //   } else {
+    //     console.log('Connection established to', url);
 
-        // do some work here with the database.
+    //     // do some work here with the database.
 
-        //Close connection
-      }
-    });
+    //     //Close connection
+    //     //db.close();
+    //   }
+    // });
 //}
 
 
