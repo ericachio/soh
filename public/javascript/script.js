da175b44e68edd0747912cc263e3372be7fd3b25
Jevons Wang <jw4106@nyu.edu>
2018-11-29 10:42:57 -0500

    stylin

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
 	$("#header").hide().delay(300).fadeIn(3000);
-	$("#follow").hide().delay(1000).fadeIn(3000);
+	$("#follow").hide().delay(800).fadeIn(3000);
 
-	var boxdelay = 1000;
+	var boxdelay = 700;
 	var increment = 400;
+
+	$(".bform").hide().delay(boxdelay+700).fadeIn(3000);
 
 	$(".btext").each(function(){
 		boxdelay += increment;
 		$(this).hide().delay(boxdelay).fadeIn(2000);
 	});
 
+	var boxdelay2 = 700
+	$(".profiletext").each(function(){
+		boxdelay2 += increment;
+		$(this).hide().delay(boxdelay2).fadeIn(2000);
+	});
+
+	$(".backbutton").hide().delay(boxdelay).fadeIn(2000);
 
 });
